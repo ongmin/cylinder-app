3f8619ac7f91253baca05e7e3cd784f13d781ba0
Min Ong <xinmin.ong@gmail.com>
2016-02-01 14:09:09 +0800

    Added React.router

--- app/components/app.js
+++ app/components/app.js
@@ -1,7 +1,7 @@
 // Disabling check because we can't run strict mode. Need global vars.
 
 var React = require('react')
-// var Header = require('./common/header')
+var Header = require('./common/header')
 // var Sidebar = require('./common/sidebar')
 var RouteHandler = require('react-router').RouteHandler
 
@@ -9,7 +9,10 @@ var App = React.createClass({
   render: function () {
     return (
       <div>
+        <Header/>
+        <div className='bodyContainer'>
         <RouteHandler/>
+        </div>
       </div>
     )
   }
