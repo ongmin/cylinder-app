e2fec5707ca20521ef28e9d4a420c4e22e78ef9c
Min Ong <xinmin.ong@gmail.com>
2016-02-02 23:52:55 +0800

    Animated video items

--- app/components/channel/playlist.js
+++ app/components/channel/playlist.js
@@ -1,6 +1,10 @@
 'use strict'
 
+global.jQuery = require('jquery')
+require('./searchResult')
+
 var React = require('react')
+
 // var Router = require('react-router')
 // var Link = Router.Link
 
@@ -8,7 +12,7 @@ var React = require('react')
 var VIDEOS = [
   {videoId: '8ELbX5CMomE', thumbnail: 'https://i.ytimg.com/vi/8ELbX5CMomE/default.jpg', title: 'Justin Bieber - Sorry (Lyric Video)', artist: 'Justin Bieber'},
   {videoId: 'rYEDA3JcQqw', thumbnail: 'https://i.ytimg.com/vi/rYEDA3JcQqw/default.jpg', title: 'Adele - Rolling in the Deep', artist: 'Adele'},
-  {videoId: 'Ri7-vnrJD3k', thumbnail: '"https://i.ytimg.com/vi/Ri7-vnrJD3k/default.jpg"', title: 'Adele - Set Fire To The Rain (Live at The Royal Albert Hall)', artist: 'Adele'}
+  {videoId: 'Ri7-vnrJD3k', thumbnail: 'https://i.ytimg.com/vi/Ri7-vnrJD3k/default.jpg', title: 'Adele - Set Fire To The Rain (Live at The Royal Albert Hall)', artist: 'Adele'}
 ]
 
 var SearchBar = React.createClass({
@@ -20,15 +24,15 @@ var SearchBar = React.createClass({
         id='search-form'>
 
         <input
-          type='search'
+          type='text'
           id='search-bar'
           placeholder='Search for videos..'
-          value=''
           ref='searchTextInput' />
 
         <button
           className='icon'
-          id='search-button' />
+          id='search-button'
+          value='Search'>Search</button>
 
       </form>
     )
@@ -42,8 +46,13 @@ var VideoItem = React.createClass({
   render: function () {
     return (
       <div className='object'>
-        'Title: ' + <td>{this.props.video.title}</td>
-        'Artist: ' + <td>{this.props.video.artist}</td>
+        <div className='object-imgbox'>
+          <img src={this.props.video.thumbnail} alt='boohoo' className='videoitem-img-responsive'/>
+          <p className='videoitem-title'>{this.props.video.title}</p>
+        </div>
+        <div className='object-textbox'>
+          <p className='videoitem-artist'>{this.props.video.artist}</p>
+        </div>
       </div>
 
     )
@@ -68,8 +77,8 @@ var VideoTable = React.createClass({
 })
 
 var Playlist = React.createClass({
-  propTypes: {
-    videos: React.PropTypes.array.isRequired
+  getInitialState: function () {
+    return {videos: []}
   },
   render: function () {
     return (
@@ -79,7 +88,7 @@ var Playlist = React.createClass({
               <SearchBar/>
             </div>
 
-            <VideoTable videos={this.props.videos} videos={VIDEOS}/>
+            <VideoTable videos={VIDEOS}/>
 
         </div>
     )
